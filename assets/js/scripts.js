185c6dc9140f9649c5a47ff900e510c154b90a75
thejenbo <jbormacoff@gmail.com>
2016-04-03 20:56:27 -0700

    tacos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -76,4 +76,25 @@ $(document).on('ready', function() {
 
 	});
 
+	// Because tacos
+	var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+
+	$(document).keydown(function(e) {
+
+		kkeys.push(e.keyCode);
+
+		if (kkeys.toString().indexOf(konami) >= 0) {
+
+			$(document).unbind('keydown', arguments.callee);
+
+			$('span.brand').text('Recovery Tacos');
+
+			$('#taco').show().animate({
+				'right': '0'
+			}, 3000);
+
+		}
+
+	});
+
 });
